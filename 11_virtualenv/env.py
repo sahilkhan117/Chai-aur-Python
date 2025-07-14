36b7b517e928b04b1500121a1f93f818ba6b0d85
Sahil Khan <sahilkhan123098p@gmail.com>
2025-07-14 21:42:52 +0530

    learn env in python

---
+++ 11_virtualenv/env.py
@@ -0,0 +1,10 @@
+# download virtualenv (pip install virtualenv)
+# create virtual enviroment:- (python -m venv .venv)
+# activate:- (./.venv/Scripts/activate)
+
+# you can install and configre this as you want the cant affect actual system
+
+# after each updation in venv update requirment.txt file (pip list > requirment.txt)
+
+# for downloading all the packages from requirment.txt file (pip install -r requirment.txt)
+
